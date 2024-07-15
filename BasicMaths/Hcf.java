a40d7498b6e0c08c8ca1d76f355bd30cfb640af5
Spoorthy-M-S <142247032+Spoorthy-M-S@users.noreply.github.com>
2024-07-15 07:01:33 +0530

    Update Hcf.java

--- BasicMaths/Hcf.java
+++ BasicMaths/Hcf.java
@@ -3,13 +3,14 @@ package BasicMaths;
 public class Hcf {
 
     public static int hcf(int a, int b) {
-
-        return -1;
+        while(a>0 && b>0){
+            if(a>b)a=a%b;
+            else b=b%a;
+        }
+        return a==0?b:a;
     }
 
     public static void main(String[] args) {
         System.out.println(hcf(16, 8));
-
     }
 
-}
